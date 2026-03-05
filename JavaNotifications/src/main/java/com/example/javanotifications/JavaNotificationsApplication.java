4d962d5ed6f3691a946df4617f1ad7ed7c921f45
cvego <cvego@Arseniy>
2026-03-05 12:01:41 +0300

    Init project

---
+++ JavaNotifications/src/main/java/com/example/javanotifications/JavaNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javanotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNotificationsApplication.class, args);
+	}
+
+}
---
+++ JavaNotifications/src/test/java/com/example/javanotifications/JavaNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javanotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
